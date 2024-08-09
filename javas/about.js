1de6b3d9da5a7c523f9debe0c0331845f7cd53cf
Ona0nline <onasihle123@gmail.com>
2024-08-09 12:24:22 +0200

    About nearly done

---
+++ javas/about.js
@@ -0,0 +1,6 @@
+// new Typewriter('#intro', {
+//   strings: '<strong>Hi! My name is Onalerona Sihle Zwane, an aspiring front-end developer.</strong><br/>I have always been drawn to the logic and precision of tech and math, but my creative side has always been a key part of who I am. I use my tech skills to bring my imaginative ideas to life, finding the perfect balance between left-brain logic and right-brain artistry. Explore this page to see how I blend creativity with technology!',
+//   autoStart: true,
+//   delay:1,
+
+// });
